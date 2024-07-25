288cf5db2db8af85c0cfa6d773c94bb1d795cfad
JK3WN <yimjay1@gmail.com>
2024-07-25 14:57:43 +0900

    Unity #D Initial Commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManagement : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    public static bool isPlaying = true;
-
     // Start is called before the first frame update
     void Start()
     {
